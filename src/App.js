ced4bbdb00ef842e85df3164a3d83754cbb59b26
sabbirzzaman <sztonmoy7@gmail.com>
2022-05-10 18:16:45 +0600

    header nav added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
+import Home from "./Pages/Home/Home";
+
 function App() {
   return (
     <div>
-      
+      <Home></Home>
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,4 +4,17 @@ module.exports = {
     extend: {},
   },
   plugins: [require("daisyui")],
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: "#3A4256",
+          secondary: "#19D3AE",
+          accent: "#0FCFEC",
+          neutral: "#3A4256",
+          "base-100": "#ffffff",
+        },
+      },
+    ],
+  },
 }
