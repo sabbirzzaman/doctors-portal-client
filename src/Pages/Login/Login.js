5fa0213d3c0018215af9ea11fe52ca0fdb386ad8
sabbirzzaman <sztonmoy7@gmail.com>
2022-05-13 17:23:48 +0600

    login form added on login page

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,10 +1,110 @@
 import React from 'react';
+import { useForm } from 'react-hook-form';
 
 const Login = () => {
+    const {
+        register,
+        formState: { errors },
+        handleSubmit,
+    } = useForm();
+
+    const onSubmit = (data) => {
+        console.log(data);
+    };
+
     return (
-        <div>
-            <h1>Login</h1>
-        </div>
+        <section>
+            <div className="mx-auto max-w-[325px] sm:max-w-[568px] md:max-w-[768px] lg:max-w-[1240px] flex justify-center items-center h-screen">
+                <div className="card w-96 bg-base-100 p-8 shadow-xl">
+                    <div className="=items-center text-center mb-5">
+                        <h2 className="text-center text-2xl font-bold">
+                            Login
+                        </h2>
+                    </div>
+
+                    <form onSubmit={handleSubmit(onSubmit)}>
+                        <div className="form-control w-full">
+                            <label className="label">
+                                <span className="label-text">Email</span>
+                            </label>
+                            <input
+                                type="text"
+                                placeholder="Type here"
+                                className="input input-bordered w-full"
+                                {...register('email', {
+                                    required: {
+                                        value: true,
+                                        message: 'Email is required',
+                                    },
+                                    pattern: {
+                                        value: /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)+$/,
+                                        message: 'Invalid email address',
+                                    },
+                                })}
+                            />
+                            <label className="label">
+                                {errors.email?.type === 'pattern' && (
+                                    <span className="label-text-alt">
+                                        {errors.email.message}
+                                    </span>
+                                )}
+                                {errors.email?.type === 'required' && (
+                                    <span className="label-text-alt">
+                                        {errors.email.message}
+                                    </span>
+                                )}
+                            </label>
+                        </div>
+
+                        <div className="form-control w-full">
+                            <label className="label">
+                                <span className="label-text">Password</span>
+                            </label>
+                            <input
+                                type="password"
+                                placeholder="Type here"
+                                className="input input-bordered w-full"
+                                {...register('password', {
+                                    required: {
+                                        value: true,
+                                        message: 'Password is required',
+                                    },
+                                    minLength: {
+                                        value: 6,
+                                        message:
+                                            'Must be 6 character or longer',
+                                    },
+                                })}
+                            />
+                            <label className="label">
+                                {errors.password?.type === 'required' && (
+                                    <span className="label-text-alt">
+                                        {errors.password.message}
+                                    </span>
+                                )}
+                                {errors.password?.type === 'minLength' && (
+                                    <span className="label-text-alt">
+                                        {errors.password.message}
+                                    </span>
+                                )}
+                            </label>
+                        </div>
+
+                        <input
+                            className="btn btn-primary w-full"
+                            type="submit"
+                            value="login"
+                        />
+
+                        <div className="divider">OR</div>
+
+                        <button className="btn btn-outline w-full">
+                            Continue With Google
+                        </button>
+                    </form>
+                </div>
+            </div>
+        </section>
     );
 };
 
